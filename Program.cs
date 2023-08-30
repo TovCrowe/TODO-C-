fe46dc44063a7d17392b35ddb65864937321f518
Victor Tovar <manuelxdvic@gmail.com>
2023-08-30 13:04:15 -0600

    Project done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("[S]ee all todos");
 Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
@@ -50,7 +52,42 @@ do {
             userChoices.Add(todoAdd);
             break;
         case "R":
-            PrintSelectedOption(userChoice); break;
+            PrintSelectedOption(userChoice);
+            if (userChoices.Count == 0)
+            {
+                Console.WriteLine("Not todos has been added, press enter to show the options again");
+            } 
+            else
+            {
+                Console.WriteLine("Which TODO want to delete, Only write the index");
+                foreach (var item in userChoices)
+                {
+                    int index = userChoices.IndexOf(item);
+                    Console.WriteLine($"{index + 1} {item}");
+                }
+                int option;
+                bool validOption = false;
+
+                do
+                {
+                    if (!int.TryParse(Console.ReadLine(), out option) || option < 1 || option > userChoices.Count)
+                    {
+                        Console.WriteLine("Invalid index. Please enter a valid index.");
+                    }
+                    else
+                    {
+                        validOption = true;
+                    }
+                } while (!validOption);
+
+                // Now 'option' contains the valid index selected by the user
+                userChoices.RemoveAt(option - 1);
+                Console.WriteLine($"TODO at index {option} has been deleted.");
+
+
+            }
+
+            break;
         case "E":
             PrintSelectedOption(userChoice); 
             Console.WriteLine("Press any key to clsoe the program");
